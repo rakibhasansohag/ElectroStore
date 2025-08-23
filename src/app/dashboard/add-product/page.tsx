daf17875fa8b0d12e05d9a78f1e9f3c65571c20c
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-08-23 14:03:11 +0600

    diploy related issue fix

--- src/app/dashboard/add-product/page.tsx
+++ src/app/dashboard/add-product/page.tsx
@@ -1,15 +1,6 @@
-import AddProductForm from '@/components/AddProductForm'; // client
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { redirect } from 'next/navigation';
+import AddProductForm from '@/components/AddProductForm';
 
 export default async function Page() {
-	const session = await getServerSession(authOptions);
-	if (!session) {
-		// server redirect -> user will never see the protected UI
-		redirect('/login');
-	}
-
 	return (
 		<main className=' px-2 md:p-6 max-w-7xl mx-auto'>
 			<h1 className='text-2xl font-bold mb-4 '>Add Product</h1>
